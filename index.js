146e8e907a87af7023e891d4814d088b4896de9b
brad-bowie <bradbowie@Brads-MacBook-Pro.local>
2015-01-14 22:39:25 -0500

    Committing redhat open shift configs

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var http = require('http');
 
-global.config = require('konfig')();
-
 var app = express();
 app.set('view engine', 'ejs'); // to load .ejs files
 app.use(express.static(__dirname + '/public'));
@@ -11,5 +9,9 @@ app.get('/', function (req, res) {
   res.render('index');
 })
 
-app.listen(global.config.env.port);
-console.log('Server on port ' + global.config.env.port);
+ 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
